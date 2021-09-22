59cfb22294d2c77ccc7ef896db77538d1cc94e7c
june7 <june7@DESKTOP-J9ECOOC>
2021-09-22 14:42:23 +0900

    1111

---
+++ kakao-1/src/main/java/com/kakao/pay/Kakao1Application.java
@@ -0,0 +1,14 @@
+package com.kakao.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kakao1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kakao1Application.class, args);
+		//test
+	}
+
+}
---
+++ kakao-1/src/test/java/com/kakao/pay/Kakao1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kakao1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
